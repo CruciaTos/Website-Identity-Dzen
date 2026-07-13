7f466dbbbf42503f01e296d71a85d1f261ceddf4
smitmhatre20 <114058957+smitmhatre20@users.noreply.github.com>
2026-07-14 02:21:13 +0530

    push

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://DZen.io";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? "https://svayatta.in";
 
 export default function robots(): MetadataRoute.Robots {
   return {
